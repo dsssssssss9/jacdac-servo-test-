bd8df25f13ec2ca51ec256a7b37c62d0ae8ae34a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-29 13:36:25 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
-input.onButtonPressed(Button.A, function () {
-    robotbit.Servo(robotbit.Servos.S8, randint(10, 160))
+radio.onReceivedNumber(function (receivedNumber) {
+    Rec_Num = receivedNumber
+    Servo_Angle = Servo_Angle + Rec_Num
+    if (Servo_Angle > 180) {
+        Servo_Angle = 180
+    } else if (Servo_Angle < 0) {
+        Servo_Angle = 0
+    }
+    robotbit.Servo(robotbit.Servos.S8, Servo_Angle)
 })
-basic.showIcon(IconNames.Heart)
-robotbit.Servo(robotbit.Servos.S1, 0)
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(robotbit.Servos.S8, 45)
+})
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(robotbit.Servos.S8, randint(0, 180))
+})
+let Rec_Num = 0
+let Servo_Angle = 0
+radio.setGroup(55)
+Servo_Angle = 90
+robotbit.Servo(robotbit.Servos.S8, Servo_Angle)
+basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
 	
 })

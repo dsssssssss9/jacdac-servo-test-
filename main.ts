ab425b13e9d937ca8345571236171e4e6852c90a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-29 12:35:35 +0100

    remove jacdac extension

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onButtonPressed(Button.A, function () {
     robotbit.Servo(robotbit.Servos.S8, randint(10, 160))
 })
 basic.showIcon(IconNames.Heart)
+robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
 	
 })
